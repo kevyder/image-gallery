1372bc8e8669da03ecfe9a51f4a9cd5d122e15a2
Kevin <kevin.rodriguez@bracksint.com>
2016-12-27 18:02:24 +0000

    Ligthbox

--- lib/image_uploader.rb
+++ lib/image_uploader.rb
@@ -1,8 +1,10 @@
 class ImageUploader < CarrierWave::Uploader::Base
     include CarrierWave::RMagick
     
+    process resize_to_fit: [720, 720]
+    
     version :small do
-        process resize_to_fit: [720, 720]
+        process resize_to_fill: [480, 480]
     end
     
 end
